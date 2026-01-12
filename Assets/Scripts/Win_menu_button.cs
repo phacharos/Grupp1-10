7dc050e0982db986389db42d89614e7ae0f393dc
phacharos <phacharos.champathong@elev.ga.lbs.se>
2026-01-12 22:13:05 +0100

    puzzle (ej klart)

--- Assets/Scripts/Win_menu_button.cs
+++ Assets/Scripts/Win_menu_button.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 
 public class Win_menu_button : MonoBehaviour
 {
-    /*public void NextScene()
-{
-    SceneManager.LoadScene("");
-}*/
+    public void NextScene()
+    {
+        SceneManager.LoadScene("");
+    }
 
 }
