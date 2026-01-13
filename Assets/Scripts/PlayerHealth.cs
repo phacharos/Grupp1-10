898ced389e44092b03ed16682c080db018df9065
adirfme <frida.teljeby@elev.ga.lbs.se>
2026-01-13 13:44:14 +0100

    forskaren kan skjuta och spelaren dör (samt lite annat)

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float health;
+    public float maxHealth;
+    public Image healthBar;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        maxHealth = health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthBar.fillAmount = Mathf.Clamp(health / maxHealth, 0, 1);
+
+        if (health <= 0 )
+        {
+            Destroy(gameObject);
+            //ladda game over scenen :)
+        }
+    }
+}
