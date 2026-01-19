fd2ce4a81406dc2c76225a60b4f19937fd4e875e
adirfme <frida.teljeby@elev.ga.lbs.se>
2026-01-19 13:09:10 +0100

    Kommenterad kod

--- Assets/Scripts/EnemyScripts/EnemyBullet.cs
+++ Assets/Scripts/EnemyScripts/EnemyBullet.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class EnemyBullet : MonoBehaviour
 {
+    // Definerar variabler
     private GameObject player;
     private Rigidbody2D rb;
     public float force;
@@ -12,9 +13,11 @@ public class EnemyBullet : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // Hittar spelaren och rigidbodyn
         rb = GetComponent<Rigidbody2D>();
         player = GameObject.FindGameObjectWithTag("Player");
 
+        // Gör så kulan åker mot spelaren samt roterar lite
         Vector3 direction = player.transform.position - transform.position;
         rb.linearVelocity = new Vector2(direction.x, direction.y).normalized * force;
 
@@ -24,14 +27,17 @@ public class EnemyBullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Skapar timer
         timer += Time.deltaTime;
 
+        // "Om det har gått mer än 10 sekunder förstörs kulan"
         if (timer > 10)
         {
             Destroy(gameObject);
         }
     }
 
+    // Om kulan kolliderar med spelaren förlorar spelaren 20 i hälsa, sedan förstörs den
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
